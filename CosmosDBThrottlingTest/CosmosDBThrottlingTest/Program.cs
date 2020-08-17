dc116bd908515dc747911b18fbe781bff2047fa4
subhrangsubhowmik <subhrangsubhowmik@gmail.com>
2020-08-17 14:04:35 +0530

    Create Program.cs

---
+++ CosmosDBThrottlingTest/CosmosDBThrottlingTest/Program.cs
@@ -0,0 +1,28 @@
+using CosmosDBThrottlingTest.Models;
+using CosmosDBThrottlingTest.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDBThrottlingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var course = new CourseRepository("MOL");
+            var AllCourses = course.GetCourses(
+                new CourseQueryParams()
+                {
+                    CourseGuid= "4ff38737-26f7-4561-bbd1-8bb3caaac778",
+                    DelFlag = false
+                }).ToList();
+
+            Console.ReadKey();
+
+        }
+    }
+}
